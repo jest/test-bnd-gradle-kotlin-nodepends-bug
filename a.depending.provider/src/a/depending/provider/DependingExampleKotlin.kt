0ee4a39fdd49018d7395c6264f9e7c5dc72a0cc3
Przemek Wesołek <przemyslaw.wesolek@interlan.pl>
2016-12-05 13:09:44 +0100

    Added an example Kotlin file.

---
+++ a.depending.provider/src/a/depending/provider/DependingExampleKotlin.kt
@@ -0,0 +1,13 @@
+package a.depending.provider
+
+import b.dependant.api.Example
+
+class DependingExampleKotlin {
+	fun doSomething(dependency: Example) {
+		
+	}
+}
+
+fun main(args: Array<String>) {
+	
+}
